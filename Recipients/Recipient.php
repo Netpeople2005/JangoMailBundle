d4627d4367542820b1fd60923b411c11202207ff
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-04 19:33:15 -0430

    clase para trabajar los destinatarios de los correos de jango

---
+++ Recipients/Recipient.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Netpeople\JangoMailBundle\Recipients;
+
+use Netpeople\JangoMailBundle\Recipients\RecipientInterface;
+
+/**
+ * Description of Recipient
+ *
+ * @author manuel
+ */
+class Recipient implements RecipientInterface
+{
+
+    protected $email;
+    protected $name;
+    
+    public function __construct($email = NULL,$name = NULL)
+    {
+        $this->setEmail($email)->setname($name);
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function setname($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    //put your code here
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+}
