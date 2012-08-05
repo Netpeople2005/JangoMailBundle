9e0140110296003400fa81ceb07308b35f74a32b
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-04 23:46:46 -0430

    los grupos poseen destinatarios

--- Groups/Group.php
+++ Groups/Group.php
@@ -2,6 +2,8 @@
 
 namespace Netpeople\JangoMailBundle\Groups;
 
+use Netpeople\JangoMailBundle\Recipients\RecipientInterface;
+
 /**
  * Description of Group
  *
@@ -10,14 +12,58 @@ namespace Netpeople\JangoMailBundle\Groups;
 class Group
 {
 
-    protected $name = '';
+    protected $name;
+    protected $groupID;
+    protected $memberCount;
 
-    public function getName(){
+    /**
+     * @var array
+     */
+    protected $recipients;
+
+    public function getName()
+    {
         return $this->name;
     }
 
-    public function setName($name){
+    public function setName($name)
+    {
         $this->name = $name;
         return $this;
     }
+
+    public function getGroupID()
+    {
+        return $this->groupID;
+    }
+
+    public function setGroupID($groupID)
+    {
+        $this->groupID = $groupID;
+        return $this;
+    }
+
+    public function getMemberCount()
+    {
+        return $this->memberCount;
+    }
+
+    public function setMemberCount($memberCount)
+    {
+        $this->memberCount = $memberCount;
+        return $this;
+    }
+
+    public function addRecipient(RecipientInterface $recipient)
+    {
+        $recipient->setGroup($this);
+        $this->recipients[$recipient->getEmail()] = $recipient;
+        return $this;
+    }
+
+    public function getRecipients()
+    {
+        return $this->recipients;
+    }
+
 }
