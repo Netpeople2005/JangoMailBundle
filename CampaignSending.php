e5324a7b99b16595b458b71a1e55d5dc64520492
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-04 23:46:31 -0430

    dando formato

--- CampaignSending.php
+++ CampaignSending.php
@@ -15,31 +15,33 @@ class CampaignSending
 {
 
     protected $jangoMail = NULL;
-    
     protected $group = NULL;
-    
     protected $email = NULL;
 
     public function __construct(JangoMail $jangoMail)
     {
         $this->jangoMail = $jangoMail;
     }
-    
-    public function setGroup(Group $group){
+
+    public function setGroup(Group $group)
+    {
         $this->group = $group;
         return $this;
     }
-    
-    public function getGroup(){
+
+    public function getGroup()
+    {
         return $this->group;
     }
-    
-    public function setEmail(EmailTemplateInterface $email){
+
+    public function setEmail(EmailTemplateInterface $email)
+    {
         $this->email = $email;
         return $this;
     }
-    
-    public function getEmail(){
+
+    public function getEmail()
+    {
         return $this->email;
     }
 
@@ -53,28 +55,33 @@ class CampaignSending
             'FromEmail' => $config['fromemail'],
             'FromName' => $config['fromname'],
             'toGroups' => $this->group->getName(),
+            'ToGroupFilter' => '', //por ahora nada
+            'ToOther' => '', //por ahora nada
+            'ToWebDatabase' => '', //por ahora nada
             'Subject' => $this->email->getSubject(),
             'MessagePlain' => $this->email->getMessagePlain(),
             'MessageHTML' => $this->email->getMessageHtml(),
-            'Options' => $this->email->getOptions(),
+            //'Options' => $this->email->getOptions(),
         );
     }
 
     public function send()
     {
-        try {
-            if ( !($this->getEmail() instanceof EmailTemplateInterface) ){
-                throw new \Exception('Debe llamar a setEmail() antes de hacer el Envío');
-            }
-            if ( !($this->getGroup() instanceof Group) ){
-                throw new \Exception('Debe llamar a setGroup() antes de hacer el Envío');
-            }
-            //var_dump($this->getParametersToSend());die;
+        if (!($this->getEmail() instanceof EmailTemplateInterface)) {
+            throw new \Exception('Debe llamar a setEmail() antes de hacer el Envío');
+        }
+        if (!($this->getGroup() instanceof Group)) {
+            throw new \Exception('Debe llamar a setGroup() antes de hacer el Envío');
+        }
+        //try {
             return $this->jangoMail->getJangoInstance()
                             ->SendMassEmail($this->getParametersToSend());
-        } catch (SoapFault $e) {
-            echo $e . '<br/>';
-        }
+//        } catch (SoapFault $e) {
+//            $this->jangoMail->setError($e->getMessage());
+//        } catch (\Exception $e) {
+//            $this->jangoMail->setError($e->getMessage());
+//        }
+        return FALSE;
     }
 
 }
