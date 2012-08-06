5ae629dd4c9b3ebac243310861568b658e4be285
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-06 15:55:34 -0430

    agregando estilos y diseño para el admin de los grupos (listas de correo)

--- Controller/GruposController.php
+++ Controller/GruposController.php
@@ -5,6 +5,7 @@ namespace Netpeople\JangoMailBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Netpeople\JangoMailBundle\Groups\Group;
+use Netpeople\JangoMailBundle\Form\Type\GroupType;
 
 /**
  * Description of GruposController
@@ -23,6 +24,31 @@ class GruposController extends Controller
         $grupos = $this->get('jango_mail')->getGroupAdmin()->getGroups();
         return array(
             'grupos' => $grupos
+        );
+    }
+
+    /**
+     * @Template()
+     * @return type 
+     */
+    public function crearAction()
+    {
+        $form = $this->createForm(new GroupType(), new Group());
+        return array(
+            'form' => $form->createView()
+        );
+    }
+
+    /**
+     * @Template()
+     * @return type 
+     */
+    public function editarAction($grupoID)
+    {
+        $form = $this->createForm(new GroupType(), new Group());
+
+        return array(
+            'form' => $form->createView()
         );
     }
 
